f55666077bd34e71ad620fcb8e764c68fc4cc92a
kpreiss37 <kpreiss@outlook.com>
2026-09-22 11:52:17 -0400

    Update ml_game_predictions.sql
    Anonymized the columns used in the regression model

--- college_basketball_pipepline+model/ml_game_predictions.sql
+++ college_basketball_pipepline+model/ml_game_predictions.sql
@@ -4,15 +4,19 @@ model_type='linear_reg',
 input_label_cols=['home_team_margin']
 ) AS
 SELECT
-home_net_diff,
-home_game,
-rest_diff,
-form_diff,
-home_team_margin,
-fg_perc_diff,
-ft_rate_diff,
-orb_perc_diff,
-to_ratio_diff
+    -- Team strength features
+    team_strength_features,
+
+    -- Game context
+    game_context_features,
+
+    -- Recent performance
+    form_features,
+
+    -- Efficiency metrics
+    efficiency_features,
+
+    home_team_margin
 FROM
 `PROJECT.DATASET.games_rated`
 WHERE 1=1
